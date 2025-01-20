92e0d627a316d0ae9bf334cf05929abd9949ca57
francoisauclair126 <francoisauclair126@gmail.com>
2025-01-19 21:28:44 -0500

    first commit

---
+++ Code.js
@@ -0,0 +1,22 @@
+import 'google-apps-script';
+
+function test2(){
+  const url = 'https://docs.google.com/spreadsheets/d/18RllQQmP_jpqn16yUnxLdE9z9zVpfHShTEdC6Z0jv9A/edit?gid=700920426#gid=700920426'
+  const ss = SpreadsheetApp.openByUrl;
+  //const sheets = ss.getSheets();
+  const sheet= ss.get;
+}
+
+function open1() {
+  const id = '18RllQQmP_jpqn16yUnxLdE9z9zVpfHShTEdC6Z0jv9A'
+  const ss = SpreadsheetApp.openById(id);
+  const sheets = ss.getSheets();
+  sheets.forEach((sheet,index)=>{
+    console.log(sheet.getSheetName());
+    sheet.setName('updated' + index);
+  })
+  
+  function test3(){
+    const b = SpreadsheetApp.getActiveSpreadsheet;
+  }
+}
