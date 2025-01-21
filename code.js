f55eb58610cf9849ad5169b25d7c6c4d9e1d6f78
francoisauclair126 <francoisauclair126@gmail.com>
2025-01-20 20:13:58 -0500

    reset

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
-function openSheet() {
-    const id = "16nnQqCLVpEgv70f-jYTICQZfCYDeNqa1foyf4YXkpFg"
-    const ss = SpreadsheetApp.openById
-    const sheet = ss.g
+function colorSheet(){
+    const id = "16nnQqCLVpEgv70f-jYTICQZfCYDeNqa1foyf4YXkpFg";
+    const ss = SpreadsheetApp.openById;
+    const sheet = ss.getSheets()[0];
+    
 }
